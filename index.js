cce098c972abea34a1fdc4a884eca8eb707b8b99
Chuck Dries <chuck@chuckdries.com>
2023-11-06 16:05:31 -0800

    add message sending

--- index.js
+++ index.js
@@ -3,12 +3,21 @@ import { engine } from 'express-handlebars';
 
 const app = express();
 
+app.use(express.urlencoded({ extended: false }))
+
 app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
 app.set('views', './views');
 
+const messages = ["this is a message hard-coded"];
+
 app.get('/', (req, res) => {
-  res.render('home');
+  res.render('home', {messages});
+})
+
+app.post('/message', (req, res) => {
+  messages.push(req.body.message)
+  res.redirect('/')
 })
 
 app.listen(8080, () => { console.log('listening on http://localhost:8080')})

ab0a7eb329a50ee7ad817ff53d61bfdc3f4f3ce1
Chuck Dries <chuck@chuckdries.com>
2023-11-06 16:42:42 -0800

    some rudimentary styling

--- index.js
+++ index.js
@@ -16,6 +16,8 @@ app.engine("handlebars", engine());
 app.set("view engine", "handlebars");
 app.set("views", "./views");
 
+app.use('/public', express.static('./public'));
+
 app.get("/", async (req, res) => {
   try {
     const db = await dbPromise;
